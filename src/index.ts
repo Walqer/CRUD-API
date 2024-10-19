4fc23c0325191ca27fa1ff55b7b0fe76e3eba94f
Alimusim <alimossim.agayev@fdrive.kz>
2024-10-20 00:24:18 +0500

    add PUT

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import http from 'http'
-import { getUsers } from './user'
+import {
+    createUser,
+    getUserById,
+    getUsers,
+    InvalidUserIdError,
+    updateUser,
+    UserNotFoundError,
+} from './user'
 const PORT = process.env.PORT || 3000
 
 const requestHandler = (
@@ -8,14 +15,111 @@ const requestHandler = (
 ) => {
     res.setHeader('Content-Type', 'application/json')
     const { method, url } = req
-    console.log(`Request received: ${method} ${url}`)
-    console.log('url', url)
+
+    if (method === 'GET' && url === '/') {
+        res.statusCode = 200
+        res.end(JSON.stringify({ message: 'Hello world' }))
+    }
+
     if (method === 'GET' && url === '/users') {
         const users = getUsers()
         res.end(JSON.stringify(users))
+        return
     }
+    if (method === 'GET' && url?.startsWith('/users/')) {
+        const id = url.split('/')[2]
 
-    res.end(JSON.stringify('Hi'))
+        try {
+            const user = getUserById(id)
+            res.end(JSON.stringify(user))
+        } catch (err) {
+            if (err instanceof InvalidUserIdError) {
+                res.statusCode = 400
+                res.end(JSON.stringify({ message: err.message }))
+            } else if (err instanceof UserNotFoundError) {
+                res.statusCode = 404
+                res.end(JSON.stringify({ message: err.message }))
+            } else {
+                res.statusCode = 500
+                res.end(
+                    JSON.stringify({ message: 'An unexpected error occurred.' })
+                )
+            }
+        }
+        return
+    }
+    if (method === 'POST' && url === '/users') {
+        let body = ''
+        req.on('data', (chunk: Buffer) => {
+            body += chunk.toString()
+        })
+        req.on('end', () => {
+            try {
+                const data = JSON.parse(body)
+                const newUser = createUser(data)
+                res.statusCode = 201
+                res.end(JSON.stringify(newUser))
+            } catch (err) {
+                if (err instanceof SyntaxError) {
+                    res.statusCode = 400
+                    res.end(
+                        JSON.stringify({
+                            message: 'Invalid request body format.',
+                        })
+                    )
+                } else if (err instanceof Error) {
+                    res.statusCode = 400
+                    res.end(JSON.stringify({ message: err.message }))
+                } else {
+                    res.statusCode = 500
+                    res.end(
+                        JSON.stringify({
+                            message: 'An unexpected error occurred.',
+                        })
+                    )
+                }
+            }
+        })
+        return
+    }
+    if (method === 'PUT' && url?.startsWith('/users/')) {
+        const id = url.split('/')[2]
+        let body = ''
+        req.on('data', (chunk: Buffer) => {
+            body += chunk.toString()
+        })
+        req.on('end', () => {
+            try {
+                const data = JSON.parse(body)
+                const updatedUser = updateUser(id, data)
+                res.end(JSON.stringify(updatedUser))
+            } catch (err) {
+                if (err instanceof SyntaxError) {
+                    res.statusCode = 400
+                    res.end(
+                        JSON.stringify({
+                            message: 'Invalid request body format.',
+                        })
+                    )
+                } else if (err instanceof InvalidUserIdError) {
+                    res.statusCode = 400
+                    res.end(JSON.stringify({ message: err.message }))
+                } else if (err instanceof UserNotFoundError) {
+                    res.statusCode = 404
+                    res.end(JSON.stringify({ message: err.message }))
+                } else {
+                    res.statusCode = 500
+                    res.end(
+                        JSON.stringify({
+                            message: 'An unexpected error occurred.',
+                        })
+                    )
+                }
+            }
+        })
+
+        return
+    }
 }
 
 const server = http.createServer(requestHandler)
--- src/user.ts
+++ src/user.ts
@@ -1,7 +1,65 @@
+import { isValidUserData } from './helpers/validateUser'
 import { User } from './types/User'
-import { validate } from 'uuid'
+import { validate, v4 as uuid } from 'uuid'
+export class InvalidUserIdError extends Error {
+    constructor(message: string) {
+        super(message)
+        this.name = 'InvalidUserIdError'
+    }
+}
+
+export class UserNotFoundError extends Error {
+    constructor(message: string) {
+        super(message)
+        this.name = 'UserNotFoundError'
+    }
+}
+const validateUser = (user: User) => {
+    const schema = {
+        type: 'object',
+        properties: {
+            age: { type: 'integer' },
+            hobbies: { type: 'array', items: { type: 'string' } },
+            username: { type: 'string' },
+        },
+    }
+}
 const users: User[] = []
 
 export const getUsers = () => {
     return users
+}
+
+export const getUserById = (id: string) => {
+    if (!validate(id)) throw new InvalidUserIdError('Invalid user ID format')
+    const user = users.find((user) => user.id === id)
+    if (!user) throw new UserNotFoundError('User not found')
+    return user
+}
+
+export const createUser = (data: Omit<User, 'id'>) => {
+    const isValid = isValidUserData(data)
+    if (isValid) {
+        const newUser = {
+            id: uuid(),
+            ...data,
+        }
+        users.push(newUser)
+        return newUser
+    }
+}
+
+export const updateUser = (id: string, data: Omit<User, 'id'>) => {
+    if (!validate(id)) throw new InvalidUserIdError('Invalid user ID format')
+    const userIndex = users.findIndex((user) => user.id === id)
+    if (userIndex === -1) throw new UserNotFoundError('User not found')
+    const isValid = isValidUserData(data)
+    if (isValid) {
+        const updatedUser = {
+            id,
+            ...data,
+        }
+        users[userIndex] = updatedUser
+        return updatedUser
+    }
 }
